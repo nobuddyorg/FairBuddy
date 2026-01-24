7a2730a98d79cdca16f6029160eb19a934d3d525
Matthias Eggert <dem.matze@gmail.com>
2026-01-24 18:12:31 +0100

    feat: add service name to config and properly use config in logger

--- services/libs/common/src/common/config.py
+++ services/libs/common/src/common/config.py
@@ -4,10 +4,12 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True, slots=True)
 class Settings:
+    service: str
     log_level: str
 
 
 def load_settings() -> Settings:
     return Settings(
+        service="fairbuddy",
         log_level=os.getenv("LOG_LEVEL", "INFO").upper(),
     )
--- services/libs/common/src/common/logging.py
+++ services/libs/common/src/common/logging.py
@@ -1,9 +1,15 @@
-import os
-
 from aws_lambda_powertools import Logger
+from .config import load_settings
 
-_logger = Logger(service="fairbuddy", level=os.getenv("LOG_LEVEL", "INFO").upper())
+_logger = None
 
 
 def get_logger():
+    global _logger
+    if _logger is None:
+        settings = load_settings()
+        _logger = Logger(
+            service=settings.service,
+            level=settings.log_level,
+        )
     return _logger
