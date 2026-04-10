f79aaa6a0c7a1892647762f1e383f0d3b81fccc0
Matthias Eggert <8039350+besessener@users.noreply.github.com>
2026-04-10 20:45:07 +0200

    feat: add buddy script as entry point

--- scripts/pulumi-deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# This script deploys the infrastructure using Pulumi.
-
-pushd "$(dirname "${BASH_SOURCE[0]}")/../infrastructure" > /dev/null || exit
-
-pulumi login s3://fairbuddy-pulumi-state
-pulumi up
-
-popd > /dev/null || exit
--- scripts/service/trigger.sh
+++ scripts/service/trigger.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+# Description: Manual trigger for a lambda function/service.
+
 set -euo pipefail
 
 # Creates a json file with the outputs of the Pulumi stack, which can be read by the services to get infrastructure details like table names, API endpoints, etc.
-pushd "$(dirname "${BASH_SOURCE[0]}")/../infrastructure" > /dev/null || exit
+pushd "$(dirname "${BASH_SOURCE[0]}")/../../infrastructure" > /dev/null || exit
   pulumi stack output --json > ../services/libs/common/src/common/.infra-outputs.json
 popd > /dev/null || exit
 
