77ce8d3da5538778f9292f077b150f2591d97811
Matthias Eggert <8039350+besessener@users.noreply.github.com>
2026-06-04 18:12:45 +0200

    feat: add image to simple start page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,10 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-2">
+      <Image src="/logo.webp" alt="FairBuddy logo" width={60} height={0} />
+
       <h1 className="text-4xl font-bold">FairBuddy</h1>
       <p className="text-gray-500">Coming soon…</p>
     </main>
