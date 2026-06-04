80651c56388639652a34e83c44a8ba6f4db070f1
Matthias Eggert <8039350+besessener@users.noreply.github.com>
2026-06-04 19:25:19 +0200

    fix: check if .secrets is there when running act

--- scripts/test/action.sh
+++ scripts/test/action.sh
@@ -3,6 +3,7 @@
 
 pushd "$(dirname "${BASH_SOURCE[0]}")/../../" > /dev/null || exit
 
+test -f .secrets || { echo "Error: .secrets file not found. Please create it with the necessary secrets." >&2; exit 1; }
 act pull_request --secret-file .secrets
 
 popd > /dev/null || exit
